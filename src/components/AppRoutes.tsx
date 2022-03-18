247ecf9252bcac938cf73c91186b6efb67db9fd4
babinmih <babinmih@mail.ru>
2022-03-18 16:30:18 +0300

    some fixes

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import { useAppSelector } from '../hooks/reduxHooks';
 import { authRoutes, publicRoutes } from '../utils/routes';
 
 const AppRoutes = () => {
-    const isAuth = true
+    const isAuth = useAppSelector(state => state.auth.isAuth)
 
     return (
         <>
             <Routes>
                 {publicRoutes.map(r => <Route path={r.path} element={r.element} key={r.path} />)}
-                
+
                 {isAuth
                     && authRoutes.map(r => <Route path={r.path} element={r.element} key={r.path} />)}
             </Routes>
